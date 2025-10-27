996cfbdbd6437b3f1728f36d525c169ae82fe439
SagWel <nicolasbensault@free.fr>
2025-10-27 15:22:22 +0100

    news

--- src/JS/scripts.js
+++ src/JS/scripts.js
@@ -1,16 +1,32 @@
 
-const movies = []
-let currentIndex = 0
+const movies = [];
 
 function card (movie) {
-    const newCard = document.createElement('div')
-    newCard.className = ''
-    newCard.innerHTML = 
-    // '
-    // Ici sera integré le HTML
-    // '
-    return newCard
+    const newCard = document.createElement('div');
+    const actorsHTML = movie.acteurs.map(acteur => `<li>${acteur}</li>`).join('');
+    newCard.className = 'smallPoster';
+    newCard.innerHTML = `
+    <img src="${movie.image}" alt="${movie.titre}" class="">
+    <div>
+        <h4>${movie.titre}</h4>
+        <p>${movie.genre}</p>
+        <p>${movie.description}</p>
+        <ul>
+            ${actorsHTML}
+        </ul>
+    </div>
+    `;
+    return newCard;
 }
+
+class carrousel {
+    constructor(containerElement) {
+        this.container = containerElement
+        this.currentIndex = 0
+        this cards = 
+    }
+}
+
 
 function displayMovies (movie) {
     const container = document.getElementById('carrousel')
@@ -21,61 +37,6 @@ function displayMovies (movie) {
     }
 }
 
-function addActorFunction () {
-    const maxActors = 10
-    const inputs = document.querySelectorAll('input[name="actorInput"]')
-    const container = document.getElementById('FormActor')
-    
-    if (inputs.length < maxActors) {
-        const newInput = document.createElement('input')
-        newInput.type = 'text'
-        newInput.name = 'actorInput'
-        newInput.className = ''
-        container.appendChild(newInput)
-    } else {
-        alert("Limite de 10 acteurs atteinte")
-        }
-}
-
-
-function newMovie (e) {
-    e.preventDefault()
-    const title = document.getElementById('titre').value.trim()
-    const description = document.getElementById('description').value.trim()
-    const genre = document.getElementById('genre').value.trim()
-    const picture = document.getElementById('image').value
-    const actorsListe = document.querySelectorAll('input[name="actorInput"]')
-    const actorsArray = []
-    const id = Date.now()
-    
-    
-    for (let actor of actorsListe) {
-        const actorValue = actor.value.trim();
-        if (actorValue != '') {
-            actorsArray.push(actorValue)
-        } else {continue}
-    }
-
-    const movie = {
-        titre: title,
-        description: description,
-        genre: genre,
-        acteurs: actorsArray,
-        image: picture,
-        id: id
-    }
-    
-    if (movie.titre =='' || movie.genre == '') {
-        alert("Manque de données essentiel")
-    } else {
-        movies.push(movie)
-        const container = document.getElementById('carrousel')
-        const newCard = card(movie)
-        container.appendChild(newCard)
-        e.target.reset()
-    }
-    
-}
 
 function carrouselPosition () {
     const container = document.getElementById('carrousel')
@@ -120,6 +81,61 @@ function left () {
     } else {
         return
     }
+}
+
+function addActorFunction () {
+    const maxActors = 10
+    const inputs = document.querySelectorAll('input[name="actorInput"]')
+    const container = document.getElementById('FormActor')
+    
+    if (inputs.length < maxActors) {
+        const newInput = document.createElement('input')
+        newInput.type = 'text'
+        newInput.name = 'actorInput'
+        newInput.className = 'actorInput'
+        container.appendChild(newInput)
+    } else {
+        alert("Limite de 10 acteurs atteinte")
+        }
+}
+
+function newMovie (e) {
+    e.preventDefault()
+    const title = document.getElementById('titre').value.trim()
+    const description = document.getElementById('description').value.trim()
+    const genre = document.getElementById('genre').value.trim()
+    const picture = document.getElementById('image').value
+    const actorsListe = document.querySelectorAll('input[name="actorInput"]')
+    const actorsArray = []
+    const id = Date.now()
+    
+    
+    for (let actor of actorsListe) {
+        const actorValue = actor.value.trim();
+        if (actorValue != '') {
+            actorsArray.push(actorValue)
+        } else {continue}
+    }
+
+    const movie = {
+        titre: title,
+        description: description,
+        genre: genre,
+        acteurs: actorsArray,
+        image: picture,
+        id: id
+    }
+    
+    if (movie.titre =='' || movie.genre == '') {
+        alert("Manque de données essentiel")
+    } else {
+        movies.push(movie)
+        const container = document.getElementById('carrousel')
+        const newCard = card(movie)
+        container.appendChild(newCard)
+        e.target.reset()
+    }
+    
 }
 
 const btnRight = document.getElementById('right')
